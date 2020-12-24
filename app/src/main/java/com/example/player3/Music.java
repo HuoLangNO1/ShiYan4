b615e59f40dca9e0302f1d72c2d3849946664caa
HL <326226122@qq.com>
2020-12-24 13:09:00 +0800

    Create Music.java

---
+++ app/src/main/java/com/example/player3/Music.java
@@ -0,0 +1,28 @@
+package com.example.player3;
+
+public class Music {
+    private String name;//歌曲名字
+    private String path;//歌曲路径
+
+    public Music(String name, String path){
+        this.name = name;
+        //this.singer = singer;
+        this.path = path;
+    }
+    public void setNameM(String nameM) {
+        this.name = nameM;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getNameM() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+}
